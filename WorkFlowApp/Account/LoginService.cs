b8c215bd014875d7cca050ea14c55afdb0ca7724
developer <ddd98754@gmail.com>
2023-06-11 21:38:21 +0900

    ログイン機能実装開始

---
+++ WorkFlowApp/Account/LoginService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowApp.Account
+{
+    internal class LoginService
+    {
+        public LoginService() { }
+
+    }
+}
--- WorkFlowApp/Account/User.cs
+++ WorkFlowApp/Account/User.cs
@@ -5,6 +5,11 @@ namespace WorkflowApp.Account
     // 申請を作成したり、承認するユーザー
     internal class User
     {
+        public User()
+        {
+
+        }
+
         private string ID { get; set; }
         private string Name { get; set; }
         private string Password { get; set; }
