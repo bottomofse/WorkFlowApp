9b32026bec010c6d4648eab608494d2cec6f5f55
developer <ddd98754@gmail.com>
2023-06-12 00:04:25 +0900

    ログイン用のリポジトリ追加（ファイルのみ）

---
+++ WorkFlowApp/Account/IAccountRepository.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace WorkflowApp.Account
+{
+    internal interface IAccountRepository
+    {
+    }
+}
---
+++ WorkFlowApp/Repository/AccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using WorkflowApp.Account;
+
+namespace WorkflowApp.Repository
+{
+    internal class AccountRepository: IAccountRepository
+    {
+
+    }
+}
